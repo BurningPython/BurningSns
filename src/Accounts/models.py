db25e23c8337a4e2ac7d8b4bcc1880926a3eddaa
julycw <julycw@gmail.com>
2013-12-23 11:10:35 +0800

    从用户模型中移除了对OpenAuth相关的操作方法

--- src/Accounts/models.py
+++ src/Accounts/models.py
@@ -4,9 +4,6 @@ from datetime import datetime
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
-from Accounts.OpenAuth.sns.tencentWeibo import (ActionHandler as tHandler)
-
-
 
 class MyUserManager(BaseUserManager):
     """
@@ -81,42 +78,6 @@ class MyUser(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
-    
-    def set_access_token(self,site_name,access_token,refresh_token,openid):
-        """
-        设置用户指定开放平台 的access_token,如果已经有该平台的信息,则更新,若没有,则添加
-        """
-        try:
-            token = self.openauth_set.get(site = site_name)
-        except:
-            token = None
-        if token:
-            token.access_token = access_token
-            token.refresh_token = refresh_token
-            token.openid = openid
-        else:
-            token = self.openauth_set.create(site = site_name, access_token = access_token, refresh_token = refresh_token, openid = openid)
-        
-        token.save()
-        
-    def get_open_auth_handler(self,site_name,request = None):
-        """
-        获取用于操作开放平台api的handler
-        """
-        try:
-            token = self.openauth_set.get(site = site_name)
-        except:
-            return None
-        else:
-            if site_name == "腾讯微博":
-                handler = tHandler(token.access_token, token.openid, request)
-            elif site_name == "新浪微博":
-#                 return OpenAuth.SinaWeibo.action.ActionHandler()
-                pass
-            else:
-                handler = None
-                
-            return handler
     
     
 class OpenAuth(models.Model):
