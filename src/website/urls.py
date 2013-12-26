8d20560cab617b8f59325faaa76bed2ecb3d9495
Eleven <iGod_eleven@163.com>
2013-12-26 20:16:49 +0800

    哈哈

--- src/website/urls.py
+++ src/website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
                        url(r'^$|^content$', views.content_view, name="content"),
+                       url(r'^$|^statuses', views.statuses_view, name="content"),
 )
