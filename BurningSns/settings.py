0273771280f29ac3bf723da8e0dba2c7caa7490d
julycw <julycw@gmail.com>
2013-12-30 15:41:53 +0800

    调整了一部分界面

--- BurningSns/settings.py
+++ BurningSns/settings.py
@@ -14,7 +14,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = (
-    u"{0}/templates".format(BASE_DIR),
+    os.path.join(BASE_DIR, "templates"),
 )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -80,7 +80,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
